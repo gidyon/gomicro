dbda2ad01b6a2cd05937dde32ece076f5de5a403
gidyon <gideon.ngk@gmail.com>
2022-10-21 07:53:27 +0300

    updates to auth

--- pkg/grpc/auth/auth.go
+++ pkg/grpc/auth/auth.go
@@ -1,14 +1,10 @@
-package auth
+package grpcauth
 
 import (
 	"context"
 	"fmt"
 	"strings"
 	"time"
-
-	"github.com/gidyon/gomicro/utils/errs"
-
-	"github.com/Pallinder/go-randomdata"
 
 	"github.com/golang-jwt/jwt"
 	"google.golang.org/grpc/codes"
@@ -19,119 +15,59 @@ import (
 	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 )
 
-// DefaultAdminGroups returns the default administrators group
-func DefaultAdminGroups() []string {
-	return []string{DefaultAdminGroup(), DefaultSuperAdminGroup()}
-}
-
-// DefaultUserGroup is the default user group
-func DefaultUserGroup() string {
-	return "USER"
-}
-
-// DefaultAdminGroup is the default admin group
-func DefaultAdminGroup() string {
-	return "ADMIN"
-}
-
-// DefaultSuperAdminGroup is the default super admin group
-func DefaultSuperAdminGroup() string {
-	return "SUPER_ADMIN"
-}
-
-// API is the interface used for authentication and authorization
-type API interface {
-	AuthenticateRequest(ctx context.Context) error
-	AuthenticateRequestV2(ctx context.Context) (*Payload, error)
-	AuthorizeGroup(ctx context.Context, allowedGroups ...string) (*Payload, error)
-	AuthorizeActor(ctx context.Context, actorID string) (*Payload, error)
-	AuthorizeActors(ctx context.Context, actorID ...string) (*Payload, error)
-	AuthorizeActorAndGroup(ctx context.Context, actorID string, allowedGroups ...string) (*Payload, error)
-	AuthorizeActorOrGroup(ctx context.Context, actorID string, allowedGroups ...string) (*Payload, error)
-	AuthorizeAdmin(ctx context.Context) (*Payload, error)
-	AuthorizeAdminStrict(ctx context.Context, adminID string) (*Payload, error)
-	AdminGroups() []string
-	AddAdminGroups(groups ...string)
-	IsAdmin(group string) bool
-	GenToken(ctx context.Context, payload *Payload, expires time.Time) (string, error)
-	GenTokenUsingKey(ctx context.Context, claims *Claims, expires time.Time, signingKey []byte) (string, error)
-	GenTokenFromClaims(ctx context.Context, claims *Claims, expires time.Time) (string, error)
-	GetJwtPayload(ctx context.Context) (*Payload, error)
-	GetPayloadFromJwt(jwt string) (*Payload, error)
-	GetClaims(ctx context.Context) (*Claims, error)
-	GetClaimsFromJwt(jwt string) (*Claims, error)
-	GetMetadataFromJwt(jwt string) (metadata.MD, error)
-	GetMetadataFromCtx(ctx context.Context) (metadata.MD, error)
-	AuthorizeFunc(ctx context.Context) (context.Context, error)
-}
-
-type authAPI struct {
-	*Options
-}
-
-// Options contains parameters for instantiating new API
-type Options struct {
-	SigningMethod    jwt.SigningMethod
-	SigningKey       []byte
-	OtherSigningKeys [][]byte
-	Issuer           string
-	Audience         string
-	AdminsGroup      []string
+type API struct {
+	signingMethod jwt.SigningMethod
+	signingKey    []byte
+	issuer        string
+	audience      string
+	adminsGroup   []string
 }
 
 // NewAPI creates a jwt authentication and authorization API using HS256 algorithm
-func NewAPI(opt *Options) (API, error) {
+func NewAPI(signingKey []byte, issuer, audience string) *API {
 
 	// Validation
 	switch {
-	case opt.SigningKey == nil:
-		return nil, errs.MissingField("jwt signing key")
-	case opt.Issuer == "":
-		return nil, errs.MissingField("jwt issuer")
-	case opt.Audience == "":
-		return nil, errs.MissingField("jwt audience")
+	case signingKey == nil:
+		panic("missing jwt signing key")
+	case issuer == "":
+		panic("missing jwt issuer")
+	case audience == "":
+		panic("missing jwt audience")
 	}
 
-	if len(opt.AdminsGroup) == 0 {
-		opt.AdminsGroup = DefaultAdminGroups()
+	api := &API{
+		signingMethod: jwt.SigningMethodHS256,
+		signingKey:    signingKey,
+		issuer:        issuer,
+		audience:      audience,
+		adminsGroup:   []string{},
 	}
 
-	opt.SigningMethod = jwt.SigningMethodHS256
-
-	optVal := *opt
-
-	api := &authAPI{Options: &optVal}
-
-	return api, nil
+	return api
 }
 
-func (api *authAPI) AuthenticateRequest(ctx context.Context) error {
-	_, err := api.ParseFromCtx(ctx)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (api *authAPI) AuthenticateRequestV2(ctx context.Context) (*Payload, error) {
-	claims, err := api.ParseFromCtx(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return claims.Payload, nil
-}
-
-func (api *authAPI) AuthorizeGroup(ctx context.Context, allowedGroups ...string) (*Payload, error) {
-	claims, ok := ctx.Value(ctxKey).(*Claims)
+// AuthorizeGroups checks whether the claims `Group` in the context `metadata.MD Authorization JWT` is a member the allowed groups set
+//
+// If it's a member, `Authorization` will succeed, otherwise it will fail with `codes.PermissionDenied`.
+//
+// The function will attempt to extract JWT token from gRPC metadata.MD `Authorization` key from the `Context`.
+//
+// If getting metadata.MD object from `Context` fails i.e due to missing metadata.MD object OR missing `Authorization` key in the metadata.MD object,
+// the function will fail with `codes.Unauthenticated`
+//
+// It is expected that before calling this method, `Authentication` ought to have happened.
+func (api *API) AuthorizeGroups(ctx context.Context, groups ...string) (*Payload, error) {
+	claims, ok := ctx.Value(claimsKey).(*Claims)
 	if !ok {
-		return nil, errs.WrapMessage(codes.Unauthenticated, "no claims found in token")
+		return nil, status.Error(codes.Unauthenticated, "no claims found in token")
 	}
 
-	err := matchGroup(claims.Payload.Group, allowedGroups)
+	// check if group match
+	err := matchGroup(claims.Payload.Group, groups)
 	if err != nil {
-		err = matchGroups(claims.Roles, allowedGroups)
+		// check with roles
+		err = matchGroups(claims.Roles, groups)
 		if err != nil {
 			return nil, err
 		}
@@ -140,174 +76,79 @@ func (api *authAPI) AuthorizeGroup(ctx context.Context, allowedGroups ...string)
 	return claims.Payload, nil
 }
 
-func (api *authAPI) AuthorizeActor(ctx context.Context, actorID string) (*Payload, error) {
-	claims, ok := ctx.Value(ctxKey).(*Claims)
+// AuthorizeIds checks whether the claims `Id` in the context `metadata.MD Authorization JWT` is a member the allowed Ids set
+//
+// If it's a member, `Authorization` will succeed, otherwise it will fail with `codes.PermissionDenied`.
+//
+// The function will attempt to extract JWT token from gRPC metadata.MD `Authorization` key from the `Context`.
+//
+// If getting metadata.MD object from `Context` fails i.e due to missing metadata.MD object OR missing `Authorization` key in the metadata.MD object,
+// the function will fail with `codes.Unauthenticated`
+//
+// It is expected that before calling this method, `Authentication` ought to have happened.
+func (api *API) AuthorizeIds(ctx context.Context, ids ...string) (*Payload, error) {
+	claims, ok := ctx.Value(claimsKey).(*Claims)
 	if !ok {
-		return nil, errs.WrapMessage(codes.Unauthenticated, "no claims found in token")
+		return nil, status.Error(codes.Unauthenticated, "no claims found in token")
 	}
 
-	if claims.ID != actorID {
-		return nil, status.Errorf(codes.PermissionDenied, "permission denied for actor with id %s", claims.ID)
-	}
-
-	return claims.Payload, nil
-}
-
-func (api *authAPI) AuthorizeActors(ctx context.Context, actorIDs ...string) (*Payload, error) {
-	claims, ok := ctx.Value(ctxKey).(*Claims)
-	if !ok {
-		return nil, errs.WrapMessage(codes.Unauthenticated, "no claims found in token")
-	}
-
-	for _, actorID := range actorIDs {
-		if claims.ID == actorID {
+	for _, id := range ids {
+		if claims.ID == id {
 			return claims.Payload, nil
 		}
 	}
 
-	return nil, status.Errorf(codes.PermissionDenied, "permission denied for actors ids [%s]", strings.Join(actorIDs, ", "))
+	return nil, status.Errorf(codes.PermissionDenied, "permission denied for actors ids [%s]", strings.Join(ids, ", "))
 }
 
-func (api *authAPI) AuthorizeActorAndGroup(ctx context.Context, actorID string, allowedGroups ...string) (*Payload, error) {
-	claims, ok := ctx.Value(ctxKey).(*Claims)
-	if !ok {
-		return nil, errs.WrapMessage(codes.Unauthenticated, "no claims found in token")
-	}
-
-	err := matchGroup(claims.Payload.Group, allowedGroups)
-	if err != nil {
-		err = matchGroups(claims.Roles, allowedGroups)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	if claims.ID != actorID {
-		return nil, status.Errorf(codes.PermissionDenied, "permission denied for actor with id %s", claims.ID)
-	}
-
-	return claims.Payload, nil
-}
-
-func (api *authAPI) AuthorizeActorOrGroup(ctx context.Context, actorID string, allowedGroups ...string) (*Payload, error) {
-	claims, ok := ctx.Value(ctxKey).(*Claims)
-	if !ok {
-		return nil, errs.WrapMessage(codes.Unauthenticated, "no claims found in token")
-	}
-
-	var err error
-	if claims.ID != actorID {
-		err = status.Errorf(codes.PermissionDenied, "permission denied for actor with id %s", claims.ID)
-	}
-
-	err1 := matchGroup(claims.Payload.Group, allowedGroups)
-	err2 := matchGroups(claims.Roles, allowedGroups)
-
-	err = hasNil(err, err1, err2)
-	if err != nil {
-		return nil, err
-	}
-
-	return claims.Payload, nil
-}
-
-func hasNil(errs ...error) error {
-	var err error
-	for _, v := range errs {
-		err = v
-		if err == nil {
-			return err
-		}
-	}
-	return err
-}
-
-func (api *authAPI) AuthorizeAdmin(ctx context.Context) (*Payload, error) {
-	claims, ok := ctx.Value(ctxKey).(*Claims)
-	if !ok {
-		return nil, errs.WrapMessage(codes.Unauthenticated, "no claims found in token")
-	}
-
-	err := matchGroup(claims.Payload.Group, api.AdminsGroup)
-	if err != nil {
-		return nil, err
-	}
-
-	return claims.Payload, nil
-}
-
-func (api *authAPI) AuthorizeAdminStrict(ctx context.Context, adminID string) (*Payload, error) {
-	claims, ok := ctx.Value(ctxKey).(*Claims)
-	if !ok {
-		return nil, errs.WrapMessage(codes.Unauthenticated, "no claims found in context")
-	}
-
-	err := matchGroup(claims.Payload.Group, api.AdminsGroup)
-	if err != nil {
-		return nil, err
-	}
-
-	if claims.ID != adminID {
-		return nil, status.Errorf(codes.PermissionDenied, "permission denied for admin with id %s", claims.ID)
-	}
-
-	return claims.Payload, nil
-}
-
-func (api *authAPI) AdminGroups() []string {
-	v := make([]string, 0, len(api.AdminsGroup))
-	v = append(v, api.AdminsGroup...)
+// AdminGroups retrieves `Admins groups` registered.
+func (api *API) AdminGroups() []string {
+	v := make([]string, 0, len(api.adminsGroup))
+	v = append(v, api.adminsGroup...)
 	return v
 }
 
-func (api *authAPI) IsAdmin(group string) bool {
-	return matchGroup(group, api.AdminsGroup) == nil
+// IsAdmin checks whether the provided `gruop` belongs to the `Admins group`.
+func (api *API) IsAdmin(group string) bool {
+	return matchGroup(group, api.adminsGroup) == nil
 }
 
-func (api *authAPI) AddAdminGroups(groups ...string) {
-	api.AdminsGroup = append(api.AdminsGroup, groups...)
-}
-
-func (api *authAPI) GenToken(ctx context.Context, payload *Payload, expirationTime time.Time) (string, error) {
+// GenToken generates JWT token with given `payload` that expire after `expirationTime` elapses.
+//
+// It uses the receivers `SigningMethod` and `SigningKey` to sign the token.
+func (api *API) GenToken(ctx context.Context, payload *Payload, expirationTime time.Time) (string, error) {
 	return api.genToken(ctx, payload, expirationTime.Unix())
 }
 
-func (api *authAPI) GenTokenUsingKey(ctx context.Context, claims *Claims, expirationTime time.Time, signingKey []byte) (string, error) {
+// GenTokenUsingKey generates JWT token with given `payload` that expire after `expirationTime` elapses.
+//
+// It uses the provided `signingKey` and the receiver `SigningMethod` to sign the token.
+func (api *API) GenTokenUsingKey(ctx context.Context, claims *Claims, expirationTime time.Time, signingKey []byte) (string, error) {
 	return api.genTokenV2(ctx, claims, expirationTime.Unix(), signingKey)
 }
 
-func (api *authAPI) GenTokenFromClaims(ctx context.Context, claims *Claims, expirationTime time.Time) (string, error) {
-	return api.genTokenV2(ctx, claims, expirationTime.Unix(), api.SigningKey)
+// GenTokenFromClaims generates JWT token with given `claims` that expire after `expirationTime` elapses.
+//
+// It uses the receivers `SigningMethod` and `SigningKey` to sign the token.
+func (api *API) GenTokenFromClaims(ctx context.Context, claims *Claims, expirationTime time.Time) (string, error) {
+	return api.genTokenV2(ctx, claims, expirationTime.Unix(), api.signingKey)
 }
 
-func (api *authAPI) GetJwtPayload(ctx context.Context) (*Payload, error) {
-	claims, ok := ctx.Value(ctxKey).(*Claims)
+// GetClaims retrives claims by reading the value of `claimsKey` in the `Context`
+func (api *API) GetClaims(ctx context.Context) (*Claims, error) {
+	claims, ok := ctx.Value(claimsKey).(*Claims)
 	if !ok {
-		return nil, errs.WrapMessage(codes.Unauthenticated, "no claims found in token")
-	}
-
-	return claims.Payload, nil
-}
-
-func (api *authAPI) GetPayloadFromJwt(jwt string) (*Payload, error) {
-	claims, err := api.ParseToken(jwt)
-	if err != nil {
-		return nil, err
-	}
-
-	return claims.Payload, nil
-}
-func (api *authAPI) GetClaims(ctx context.Context) (*Claims, error) {
-	claims, ok := ctx.Value(ctxKey).(*Claims)
-	if !ok {
-		return nil, errs.WrapMessage(codes.Unauthenticated, "no claims found in token")
+		return nil, status.Error(codes.Unauthenticated, "no claims found in token")
 	}
 
 	return claims, nil
 }
 
-func (api *authAPI) GetClaimsFromJwt(jwt string) (*Claims, error) {
-	claims, err := api.ParseToken(jwt)
+// GetClaimsFromJwt retrives claims by parsing the `jwt` string.
+//
+// It uses the reciever `SigningKey` during parsing.
+func (api *API) GetClaimsFromJwt(jwt string) (*Claims, error) {
+	claims, err := api.parseToken(jwt)
 	if err != nil {
 		return nil, err
 	}
@@ -315,11 +156,13 @@ func (api *authAPI) GetClaimsFromJwt(jwt string) (*Claims, error) {
 	return claims, nil
 }
 
-func (api *authAPI) GetMetadataFromJwt(jwt string) (metadata.MD, error) {
+// GetMetadataFromJwt creates a metadata.MD object from `jwt` string.
+func (api *API) GetMetadataFromJwt(jwt string) (metadata.MD, error) {
 	return metadata.Pairs(Header(), fmt.Sprintf("%s %s", Scheme(), jwt)), nil
 }
 
-func (api *authAPI) GetMetadataFromCtx(ctx context.Context) (metadata.MD, error) {
+// GetMetadataFromCtx retrieves metadata.MD object from `Context`
+func (api *API) GetMetadataFromCtx(ctx context.Context) (metadata.MD, error) {
 	token, err := grpc_auth.AuthFromMD(ctx, "bearer")
 	if err != nil {
 		return nil, err
@@ -327,36 +170,38 @@ func (api *authAPI) GetMetadataFromCtx(ctx context.Context) (metadata.MD, error)
 	return metadata.Pairs(Header(), fmt.Sprintf("%s %s", Scheme(), token)), nil
 }
 
-func (api *authAPI) AuthorizeFunc(ctx context.Context) (context.Context, error) {
+// Authenticator is the function that performs authentication
+//
+// The passed in `Context` will contain the gRPC metadata.MD object (for header-based authentication) and
+// the peer.Peer information that can contain transport-based credentials (e.g. `credentials.AuthInfo`).
+//
+// The returned context will be propagated to handlers, allowing user changes to `Context`. However,
+// please make sure that the `Context` returned is a child `Context` of the one passed in.
+//
+// If error is returned, its `grpc.Code()` will be returned to the user as well as the verbatim message.
+// Please make sure you use `codes.Unauthenticated` (lacking auth) and `codes.PermissionDenied`
+func (api *API) Authenticator(ctx context.Context) (context.Context, error) {
 	token, err := grpc_auth.AuthFromMD(ctx, "bearer")
 	if err != nil {
 		return nil, err
 	}
 
-	claims, err := api.ParseToken(token)
+	claims, err := api.parseToken(token)
 	if err != nil {
 		return nil, status.Errorf(codes.Unauthenticated, "invalid auth token: %v", err)
 	}
 
 	grpc_ctxtags.Extract(ctx).Set("auth.sub", userClaimFromToken(claims))
 
-	return context.WithValue(ctx, ctxKey, claims), nil
+	return context.WithValue(ctx, claimsKey, claims), nil
 }
 
-// ParseFromCtx jwt token from context
-func (api *authAPI) ParseFromCtx(ctx context.Context) (*Claims, error) {
-	token, err := grpc_auth.AuthFromMD(ctx, "Bearer")
-	if err != nil {
-		return nil, status.Errorf(
-			codes.PermissionDenied, "failed to get Bearer token from authorization header: %v", err,
-		)
-	}
-
-	return api.ParseToken(token)
+func userClaimFromToken(claims *Claims) *Payload {
+	return claims.Payload
 }
 
-// ParseToken parses a jwt token and return claims or error if token is invalid
-func (api *authAPI) ParseToken(tokenString string) (claims *Claims, err error) {
+// parses a jwt token and return claims or error if token is invalid
+func (api *API) parseToken(tokenString string) (claims *Claims, err error) {
 	// Handling any panic is good trust me!
 	defer func() {
 		if err2 := recover(); err2 != nil {
@@ -368,7 +213,7 @@ func (api *authAPI) ParseToken(tokenString string) (claims *Claims, err error) {
 		tokenString,
 		&Claims{},
 		func(token *jwt.Token) (interface{}, error) {
-			return api.SigningKey, nil
+			return api.signingKey, nil
 		},
 	)
 	if err != nil {
@@ -383,44 +228,13 @@ func (api *authAPI) ParseToken(tokenString string) (claims *Claims, err error) {
 	return claims, nil
 }
 
-func userClaimFromToken(claims *Claims) *Payload {
-	return claims.Payload
-}
-
 type claims string
 
-// ctxKey holds the context key containing the token information
-const ctxKey = claims("claims")
+// claimsKey holds the context key containing the token information
+const claimsKey = claims("claims")
 
-// AddMD adds metadata to token
-func (api *authAPI) AddMD(ctx context.Context, actorID, group string) context.Context {
-	payload := &Payload{
-		ID:           actorID,
-		Names:        randomdata.SillyName(),
-		EmailAddress: randomdata.Email(),
-		Group:        group,
-	}
-	token, err := api.genToken(ctx, payload, 0)
-	if err != nil {
-		panic(err)
-	}
-
-	return addTokenMD(ctx, token)
-}
-
-// AddTokenMD adds token as authorization metadata to context and returns the updated context object
-func AddTokenMD(ctx context.Context, token string) context.Context {
-	return addTokenMD(ctx, token)
-}
-
-func addTokenMD(ctx context.Context, token string) context.Context {
-	return metadata.NewIncomingContext(
-		ctx, metadata.Pairs("authorization", fmt.Sprintf("Bearer %s", token)),
-	)
-}
-
-func matchGroup(claimGroup string, allowedGroups []string) error {
-	for _, group := range allowedGroups {
+func matchGroup(claimGroup string, groups []string) error {
+	for _, group := range groups {
 		if claimGroup == group {
 			return nil
 		}
@@ -428,8 +242,8 @@ func matchGroup(claimGroup string, allowedGroups []string) error {
 	return status.Errorf(codes.PermissionDenied, "permission denied for group %s", claimGroup)
 }
 
-func matchGroups(claimGroups []string, allowedGroups []string) error {
-	for _, group := range allowedGroups {
+func matchGroups(claimGroups []string, groups []string) error {
+	for _, group := range groups {
 		for _, claimGroup := range claimGroups {
 			if claimGroup == group {
 				return nil
